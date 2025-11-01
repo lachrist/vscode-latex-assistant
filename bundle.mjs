2c9cbaa2e1e77068ac07bab133e9721b32914016
Laurent Christophe <laurent.christophe.terken@gmail.com>
2025-11-01 10:57:09 +0100

    fix: respect new constraint on esbuild filtering

--- bundle.mjs
+++ bundle.mjs
@@ -1,23 +1,31 @@
 import esbuild from "esbuild";
+import pack from "./package.json" with { type: "json" };
 
-const external = /\/ext\/([^/]*)\.mjs$/gu;
+const main = "./out/index.js";
 
-esbuild
-  .build({
-    entryPoints: ["src/index.mjs"],
-    bundle: true,
-    outfile: "out/index.js",
-    platform: "node",
-    plugins: [
-      {
-        name: "external-resolved",
-        setup: (build) => {
-          build.onResolve({ filter: external }, ({ path }) => {
-            const parts = external.exec(path);
-            return { external: true, path: parts[1] };
-          });
-        },
+if (pack.main !== main) {
+  throw new Error(
+    `package.json "main" field should be "${main}" in production.`,
+    { cause: pack.main },
+  );
+}
+
+const external = /\/ext\/([^/]*)\.mjs$/;
+
+await esbuild.build({
+  entryPoints: ["src/index.mjs"],
+  bundle: true,
+  outfile: "out/index.js",
+  platform: "node",
+  plugins: [
+    {
+      name: "external-resolved",
+      setup: (build) => {
+        build.onResolve({ filter: external }, ({ path }) => {
+          const parts = external.exec(path);
+          return { external: true, path: parts[1] };
+        });
       },
-    ],
-  })
-  .catch(() => process.exit(1));
+    },
+  ],
+});
